8dcbe913f60fe390dd680095096c464d1f6c0fc1
Harsh <harsh@hkumar.me>
2017-09-22 09:29:41 -0700

    Create seed file for demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'John Doe',
+  username: 'johndoe',
+  password: 'password',
+  company: "John's Dough Company",
+  email: 'john@doe.com')
+user.save
+# Company.create(name: "John's Dough Company")
+project = Project.create(name: 'Company HQ',
+  description: 'Company-wide announcements and stuff everyone needs to know',
+  admin_id: user.id,
+  project_type: 'company')
+
+user_project = UserProject.create(user_id: user.id, project_id: project.id)
