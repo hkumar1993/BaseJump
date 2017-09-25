21c216126ca12777ee56c3d76581bce326089075
Harsh <harsh@hkumar.me>
2017-09-25 13:50:01 -0700

    Update seeds

--- db/migrate/20170925193722_add_assignee_ids.rb
+++
@@ -1,5 +0,0 @@
-class AddAssigneeIds < ActiveRecord::Migration[5.1]
-  def change
-    add_column :todos, :assignee_users, :string, array: true, default: []
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: 'John Doe',
-  username: 'johndoe',
-  password: 'password',
-  company: "John's Dough Company",
-  email: 'john@doe.com')
-user.save
-# Company.create(name: "John's Dough Company")
-project = Project.create(name: 'Company HQ',
-  description: 'Company-wide announcements and stuff everyone needs to know',
-  admin_id: user.id,
-  project_type: 'company')
+john = User.create!(name: 'John Doe',
+username: 'johndoe',
+password: 'password',
+company: "John's Dough Company",
+email: 'john@doe.com')
 
-user_project = UserProject.create(user_id: user.id, project_id: project.id)
+jane = User.create!(name: 'Jane Doe',
+username: 'janedoe',
+password: 'password',
+company: "John's Dough Company",
+email: 'jane@doe.com')
 
-user = User.create(name: 'Jane Doe',
-  username: 'janedoe',
-  password: 'password',
-  company: "John's Dough Company",
-  email: 'jane@doe.com')
+hq = Project.create!(name: 'Company HQ',
+description: 'Company-wide announcements and stuff everyone needs to know',
+admin_id: john.id,
+project_type: 'company')
 
-user.save
-user_project = UserProject.create(user_id: user.id, project_id: project.id)
+hr = Project.create!(name: 'Human Resources',
+  description: 'Hirings and Firings',
+  admin_id: john.id,
+  project_type: 'team')
 
-todo_list = TodoList.create(title: 'Our first todo list',
+finance = Project.create!(name: 'Finance',
+  description: 'Provide the bread and butter',
+  admin_id: john.id,
+  project_type: 'team')
+
+it = Project.create!(name: 'Create IT Department',
+  description: 'Collaboration with HR and Finance to plan IT hirings',
+  admin_id: john.id,
+  project_type: 'project')
+
+
+(1..2).each do |id|
+  UserProject.create!(user_id: id, project_id: hq.id)
+  UserProject.create!(user_id: id, project_id: hr.id)
+  UserProject.create!(user_id: id, project_id: finance.id)
+  UserProject.create!(user_id: id, project_id: it.id)
+end
+
+todo_list = TodoList.create!(title: 'The basics',
 description: 'This is the companies first todo list',
 author_id: 1,
 project_id: 1)
 
-todo = Todo.create(title: 'First todo',
+todo = Todo.create!(title: 'Create company logo',
   author_id: 1,
   todo_list_id: 1
 )
-
-user_todo = UserTodo.create(user_id: 1, todo_id: 1)
+todo = Todo.create!(title: 'Create company mission and vision',
+  author_id: 1,
+  todo_list_id: 1
+)
+todo = Todo.create!(title: 'Third todo',
+  author_id: 1,
+  todo_list_id: 1
+)
