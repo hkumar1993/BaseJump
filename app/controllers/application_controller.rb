2a108652e666a7b779f9e0f1597492e7e77f4524
Harsh <harsh@hkumar.me>
2017-11-13 18:52:39 -0800

    Add current company and current projects to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_company, :current_projects
 
   def current_user
     User.find_by(session_token: session[:session_token])
   end
 
+
   def logged_in?
     !!current_user
   end
 
+  def current_company
+    return session[:business] = current_user.business if logged_in?
+    nil
+  end
+
+  def current_projects
+    return session[:projects] = current_user.projects if logged_in?
+    nil
+  end
+
   def login!(user)
     session[:session_token] = user.session_token
+    session[:business] = user.business
+    session[:projects] = user.projects
   end
 
   def logout!
