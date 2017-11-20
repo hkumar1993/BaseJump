df4106b174f06dcfa47eeb2f06a6e5aef9e255f0
Harsh <harsh@hkumar.me>
2017-11-19 23:03:08 -0800

    Set up landing page image loaders

--- frontend/components/landing_page/nav_bar.jsx
+++ frontend/components/landing_page/nav_bar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import BasecampLogo from '../../images/basejump_logo.svg'
 
 const NavBar = (props) => {
   return (
   <nav className='landing-nav'>
     <Link to='/' className='logo'>
-      <img src='https://3.basecamp-static.com/bcxhq/assets/about/press/standard-basecamp-logo-ab9e0d21981d719a3c08c3b24fce0028f7cfd9d8af4106062bb55347967e6e9a.svg'/>
+      <img src={BasecampLogo}/>
     </Link>
     <ul>
       <li>
--- frontend/components/landing_page/page_content.jsx
+++ frontend/components/landing_page/page_content.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import BasecampImage from '../../images/basejump_image.jpg'
 
 const PageContent = () => (
   <div className='landing-page-main'>
-    <img src='https://3.basecamp-static.com/bcxhq/assets/landing/jtbd/hof-cropped@2x-39cc220172880bf82747f19781a38cb6b3343f2b932bd3aab5742df603910be2.jpg'/>
+    <img src={BasecampImage}/>
     <h1>We've been expecting you.</h1>
     <p>All growing businesses run into the same fundamental problems. Hair on fire, buried under email, stuff everywhere. The good news? <a href='#'>Basejump solves them.</a></p>
   </div>
