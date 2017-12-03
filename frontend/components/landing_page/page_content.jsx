13edb7d37aac5b117862d1c8ee3c3e4fe8b0e6cd
Harsh <harsh@hkumar.me>
2017-12-02 19:12:01 -0800

    Make url of images constant

--- frontend/components/landing_page/page_content.jsx
+++ frontend/components/landing_page/page_content.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import BasecampImage from '../../images/basejump_image.jpg'
+// import BasecampImage from '../../images/basejump_image.jpg'
+
+const BasecampImage = 'https://3.basecamp-static.com/bcxhq/assets/landing/jtbd/hof-cropped@2x-39cc220172880bf82747f19781a38cb6b3343f2b932bd3aab5742df603910be2.jpg'
 
 const PageContent = () => (
   <div className='landing-page-main'>
     <img src={BasecampImage}/>
     <h1>We've been expecting you.</h1>
-    <p>All growing businesses run into the same fundamental problems. Hair on fire, buried under email, stuff everywhere. The good news? <a href='#'>Basejump solves them.</a></p>
+    <p>All growing businesses run into the same fundamental problems. Hair on fire, buried under email, stuff everywhere. The good news? <a href='#/login'>Basejump solves them.</a></p>
   </div>
 )
 
